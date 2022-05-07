cc7625ff4e18509bf2bb1686f2a29c11d725195e
sinzianastanciu <sinzianastanciu98@gmail.com>
2022-05-07 16:40:24 +0300

    add table component and modify existing components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Button = ({ children }) => {
-    return (
-        <button>
-            {children}
-        </button>
-    );
+const Button = ({ children, className, ...rest }) => {
+  return (
+    <button className={`button ${className}`} {...rest}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
