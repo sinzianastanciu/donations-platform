cd49c5852a2695b03bb50e106c04b9aac81e3777
iustinablaga <87121288+iustinablaga@users.noreply.github.com>
2022-05-07 19:16:54 +0300

    User home page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ children }) => {
     return (
-        <button>
+        <button className="button">
             {children}
         </button>
     );
