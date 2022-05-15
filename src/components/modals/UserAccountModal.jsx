c6ee96751845cd6235d7d3e4c85d41fdc4572bc4
sinzianastanciu <sinzianastanciu98@gmail.com>
2022-05-15 19:45:04 +0300

    profile page + modal for editing profile data

--- src/components/modals/UserAccountModal.jsx
+++ src/components/modals/UserAccountModal.jsx
@@ -1,41 +1,45 @@
-import React from 'react';
-import Modal from 'react-modal';
-import Button from '../Button';
-import Input from '../Input';
-import { MdOutlineClose } from 'react-icons/md';
+import React from "react";
+import Modal from "react-modal";
+import Button from "../Button";
+import Input from "../Input";
+import { MdOutlineClose } from "react-icons/md";
 
 const UserAccountModal = ({ isModalOpen, closeModal }) => {
-    return (
-        <Modal 
-            isOpen={isModalOpen}
-            onRequestClose={closeModal}
-            contentLabel="Edit account"
-            className="modal"
-        >
-            <div className="modal-header">
-                <h2>Edit account details</h2>
-                <Button onClick={closeModal} className="icon-button">
-                    <MdOutlineClose/>
-                </Button>
+  return (
+    <Modal
+      isOpen={isModalOpen}
+      onRequestClose={closeModal}
+      contentLabel="Edit account"
+      className="user-modal"
+    >
+      <div className="row-between">
+        <h2>Edit account details</h2>
+        <Button onClick={closeModal} className="icon-button">
+          <MdOutlineClose />
+        </Button>
+      </div>
+      <div className="line"/>
+        <form>
+          <div className="modal-fields">
+            <div className="row-inputs">
+              <Input label="Email" placeholder="Email address"></Input>
+              <Input label="Phone" placeholder="Phone number"></Input>
+              <Input label="FirstName" placeholder="First Name"></Input>
+              <Input label="LastName" placeholder="Last Name"></Input>
             </div>
-            <div className="modal-content">
-                <form>
-                    <Input label="Email" placeholder="Email address"></Input>
-                    <Input label="Phone" placeholder="Phone number"></Input>
-                    <Input label="FirstName" placeholder="First Name"></Input>
-                    <Input label="LastName" placeholder="Last Name"></Input>
-                    <Input label="City" placeholder="City"></Input>
-                    <Input label="Country" placeholder="Country"></Input>
-                    <Input label="Address" placeholder="Address"></Input>
-                    <Input label="Password" placeholder="Password"></Input>
-                    <Button type="button" onClick={closeModal}>
-                        Save
-                    </Button>
-                </form>
+            <div className="row-inputs">
+              <Input label="City" placeholder="City"></Input>
+              <Input label="Country" placeholder="Country"></Input>
+              <Input label="Address" placeholder="Address"></Input>
+              <Input label="Password" placeholder="Password"></Input>
             </div>
-
-        </Modal>
-    );
+          </div>
+          <Button type="button" onClick={closeModal}>
+            Save
+          </Button>
+        </form>
+    </Modal>
+  );
 };
 
 export default UserAccountModal;
--- src/pages/user/Account.jsx
+++ src/pages/user/Account.jsx
@@ -1,11 +1,90 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
+import Button from "../../components/Button";
+import UserLayout from "../../utils/UserLayout";
+import Section from "../../components/Section.jsx";
+import Table from "../../components/Table";
+import UserAccountModal from "../../components/modals/UserAccountModal";
 
-const Account = () =>  {
-    return (
-        <div>
-            <h1>User Info</h1>
+const Account = () => {
+  const [openedModal, setOpenedModal] = useState(false);
+  const profileFields = [
+    { key: "Name", value: "Jake Markel" },
+    { key: "Email", value: "markel.jake@gmail.com" },
+    { key: "Phone", value: "+40 754 342 3223" },
+    { key: "Address", value: "Street Lake 64" },
+  ];
+
+  const columns = [
+    {
+      Header: "Cause name",
+      accessor: "cause_title",
+    },
+    {
+      Header: "Amount",
+      accessor: "amount",
+    },
+    {
+      Header: "Time",
+      accessor: "time",
+    },
+  ];
+
+  const data = useMemo(
+    () => [
+      {
+        cause_title: "Kids from Ukraine",
+        amount: 1000,
+        time: "10 March 2021"
+      },
+      {
+        cause_title: "Kids from Ukraine",
+        amount: 1000,
+        time: "10 March 2021"
+      },
+      {
+        cause_title: "Kids from Ukraine",
+        amount: 1000,
+        time: "10 March 2021"
+      },
+      {
+        cause_title: "Kids from Ukraine",
+        amount: 1000,
+        time: "10 March 2021"
+      },
+      {
+        cause_title: "Kids from Ukraine",
+        amount: 1000,
+        time: "10 March 2021"
+      },
+    ],
+    []
+  );
+
+  return (
+    <UserLayout>
+      <UserAccountModal
+        isModalOpen={openedModal}
+        closeModal={() => {
+          setOpenedModal(false);
+        }}
+      />
+      <div className="profile">
+        <div className="profile-title">
+          <h2>Jake Markel</h2>
+          <Button onClick={() => setOpenedModal(true)}>
+             Edit
+          </Button>
         </div>
-    );
+        <div className="profile-content">
+          <Section title={"Profile Details"} fields={profileFields} />
+          <div>
+            <p className="section-title">Past Donations</p>
+            <Table data={data} columns={columns} noHref />
+          </div>
+        </div>
+      </div>
+    </UserLayout>
+  );
 };
 
 export default Account;
