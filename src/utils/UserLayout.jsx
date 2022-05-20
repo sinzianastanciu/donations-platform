9df20799bfc41e0b0f0cebe579a9d28ceb39c469
iustinablaga <87121288+iustinablaga@users.noreply.github.com>
2022-05-20 11:46:08 +0300

    Add backend call for add users

--- src/utils/UserLayout.jsx
+++ src/utils/UserLayout.jsx
@@ -1,18 +1,28 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import { authSettings } from "../AuthSettings";
 import UserHeader from "./UserHeader";
+import axiosInstance from "../configurations/axiosInstance";
+import { routes } from "../configurations/api"
 
 const UserLayout = ({ children }) => {
   const { user } = useAuth0();
   const navigate = useNavigate();
 
+  const addUser = useCallback(async () => {
+    axiosInstance.post(routes.profile.setupProfile, {
+      email: user.email
+    });
+  }, []);
+
+
   useEffect(() => {
     if (user && user[authSettings.rolesKey].length === 1) {
       navigate("/causes");
     }
-  }, [user]);
+    addUser();
+  }, [user, addUser]);
 
   return (
     <div className="user-layout">
